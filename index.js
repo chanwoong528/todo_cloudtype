2856736a62f44001806fc79a44e0c54c280828aa
문찬웅 <mooncw@hankookilbo.com>
2023-01-10 17:04:42 +0900

    netlify domain added

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const PORT = process.env.PORT || 5000;
 app.use(
     cors({
         origin: [
+            "https://moontododemo.netlify.app/",
             "http://localhost:3000",
         ],
         credentials: true,
